cfef9fac1acbc78e0f4e05149770d73a8e14f4f4
Molfar <52976230+MolfarUA@users.noreply.github.com>
2021-07-28 18:41:12 +0300

    Create Extra-Terrestrials.java

---
+++ Easy/Extra-Terrestrials/Extra-Terrestrials.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ExtraTerrestrials {
+    public static void main(final String[] args) {
+        System.out.print(
+            new StringBuilder(
+                new Scanner(System.in).nextLine()
+            ).reverse()
+        );
+    }
+}
