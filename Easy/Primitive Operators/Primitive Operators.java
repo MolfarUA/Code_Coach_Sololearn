f12309f8527544b501fd68f07b447b54e88cd74f
Molfar <52976230+MolfarUA@users.noreply.github.com>
2021-07-28 18:53:45 +0300

    Create Primitive Operators.java

---
+++ Easy/Primitive Operators/Primitive Operators.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+
+   public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+       int math = scanner.nextInt();
+       int history = scanner.nextInt();
+       int geometry = scanner.nextInt();
+   }
+}
