74f8499b7c8bae90edf404d32941a72731916615
Molfar <52976230+MolfarUA@users.noreply.github.com>
2021-07-28 23:43:50 +0300

    Create Military Time.java

---
+++ Medium/Military Time/Military Time.java
@@ -0,0 +1,20 @@
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Locale;
+import java.util.Scanner;
+
+public final class MilitaryTime {
+    public static void main(String[] args) {
+        final var scanner = new Scanner(System.in);
+        final var usTime = scanner.nextLine();
+        
+        final var formatter = DateTimeFormatter
+                .ofLocalizedTime(FormatStyle.SHORT)
+                .withLocale(Locale.US);
+
+        final var euTime = LocalTime.parse(usTime, formatter);
+        System.out.println(euTime);
+    }
+}
+© 2021 GitHub, Inc.
