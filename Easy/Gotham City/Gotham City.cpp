6513f1fe068ebda0942c1f3a2534de768db9539c
Molfar <52976230+MolfarUA@users.noreply.github.com>
2021-07-28 17:28:07 +0300

    Create Gotham City.cpp

---
+++ Easy/Gotham City/Gotham City.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int input;
+    cin>>input;
+    if(input <5)cout<<"I got this!";
+    else if(input>=5 && input<10)cout<<"Help me Batman";
+    else cout<<"Good Luck out there!";
+    return 0;
+}
