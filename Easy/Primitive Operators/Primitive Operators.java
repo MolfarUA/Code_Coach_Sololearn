8670b3a74a0b7c22960d2269a6b947580e92f2e1
Molfar <52976230+MolfarUA@users.noreply.github.com>
2021-07-28 20:43:36 +0300

    Update Primitive Operators.java

--- Easy/Primitive Operators/Primitive Operators.java
+++ Easy/Primitive Operators/Primitive Operators.java
@@ -7,5 +7,22 @@ public class Main {
        int math = scanner.nextInt();
        int history = scanner.nextInt();
        int geometry = scanner.nextInt();
+       
+       //your code goes here
+       int totalTimeSpent = 
+       math + history + geometry;
+       
+       int hourValue = 60;
+       
+       int firstLineValue = totalTimeSpent / hourValue;
+       
+       int secondLineValue = totalTimeSpent % hourValue;
+       
+       System.out.println(firstLineValue);
+       
+       System.out.println(secondLineValue);
+    
+       
+       
    }
 }
