7234959ce99527487bea6961116e8a3bb87dd6ed
Molfar <52976230+MolfarUA@users.noreply.github.com>
2021-07-28 17:28:55 +0300

    Create Gotham City.swift

---
+++ Easy/Gotham City/Gotham City.swift
@@ -0,0 +1,5 @@
+if let input = readLine(), let val = Int(input) {
+    if(val<5){print("I got this!")}
+    else if(val>=5 && val<10){print("Help me Batman")}
+    else{print("Good Luck out there!")}
+}
