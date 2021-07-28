2966b5af1c3e43be335179d5a42e5c0d66ecc164
Molfar <52976230+MolfarUA@users.noreply.github.com>
2021-07-28 17:24:40 +0300

    Create Gotham City.py

---
+++ Easy/Gotham City/Gotham City.py
@@ -0,0 +1,8 @@
+criminals = int(input())
+
+if (criminals < 5):
+    print('I got this!')
+elif (criminals >= 5 and criminals <=10):
+    print('Help me Batman')
+else:
+    print('Good Luck out there!')
