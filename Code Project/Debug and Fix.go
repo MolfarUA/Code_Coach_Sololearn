434da456a06cbc877d17659ef46ba3d82dd3b006
Molfar <52976230+MolfarUA@users.noreply.github.com>
2021-07-30 20:43:26 +0300

    Create Debug and Fix.go

---
+++ Code Project/Debug and Fix.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+//import "strings"
+func main() {
+   //outputs GO 3 times
+   for i:=1; i<=3; i++ {
+    //fmt.Printline("Go\n")
+    fmt.Println("GO")
+    //fmn.Println("0")
+}
+}
