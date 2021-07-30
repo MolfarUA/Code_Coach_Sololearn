5137c082b0463d0bfc754c5efdbdf64908ae23e0
Molfar <52976230+MolfarUA@users.noreply.github.com>
2021-07-30 21:24:00 +0300

    Create Fibonacci.py

---
+++ Code Project/Fibonacci.py
@@ -0,0 +1,11 @@
+num = int(input())
+def fibonacci(n):
+    if n <= 1:
+        return n
+
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+
+for number in range(num):
+    print(fibonacci(number))
