79c41353939ea93e3d3d3313dddae6af0eeb43b1
Molfar <52976230+MolfarUA@users.noreply.github.com>
2021-07-30 21:01:21 +0300

    Create COVID Data Analysis.py

---
+++ Code Project/COVID Data Analysis.py
@@ -0,0 +1,7 @@
+import pandas as pd
+df = pd.read_csv("/usercode/files/ca-covid.csv")
+df.drop('state', axis=1, inplace=True)
+df.set_index('date', inplace=True)
+df['ratio'] = df['deaths'] / df['cases']
+y = df['ratio'].max()
+print(df[df['ratio'] == y])
