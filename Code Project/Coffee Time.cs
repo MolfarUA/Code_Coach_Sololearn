31f7b24595214804880993dc05a48e61bad7a1da
Molfar <52976230+MolfarUA@users.noreply.github.com>
2021-08-02 20:21:44 +0300

    Create Coffee Time.cs

---
+++ Code Project/Coffee Time.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int discount = Convert.ToInt32(Console.ReadLine());
+
+            Dictionary<string, int> coffee = new Dictionary<string, int>();
+            coffee.Add("Americano", 50);
+            coffee.Add("Latte", 70);
+            coffee.Add("Flat White", 60);
+            coffee.Add("Espresso", 60);
+            coffee.Add("Cappuccino", 80);
+            coffee.Add("Mocha", 90);
+
+
+            //your code goes here
+foreach (KeyValuePair<string, int> entry in coffee)
+            {
+                Console.WriteLine(entry.Key + ": " + (entry.Value - entry.Value * discount / 100));
+            }
+
+
+        }
+    }
+}
